795d83764ab853289e496a9fd9ad7a79cbee556d
David Mears <kieukmail@gmail.com>
2019-05-15 14:38:03 +0100

    Enhance tests and have homepage display the most recent to-do added

--- lists/tests.py
+++ lists/tests.py
@@ -9,3 +9,7 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_POST_request_data_can_be_saved(self):
+        response = self.client.post('/', data={'item_text': 'Test to-do'})
+        self.assertIn('Test to-do', response.content.decode())
